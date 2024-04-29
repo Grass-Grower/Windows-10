f45916054c37b88438be647373eb191da407c974
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-28 20:55:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install qemu-kvm qemu-system-x86 openbox firefox tigervnc-standalone-server
 git clone https://github.com/novnc/noVNC
 cd noVNC
-sudo vncserver -SecurityType none -xstartup "openbox" -r fbport 5900
+sudo vncserver -SecurityType none -xstartup "openbox" -rfbport 5900
+sudo ./utils/novnc_proxy/ --vnc 127.0.0.1:5900 --listen to calhost:6000
